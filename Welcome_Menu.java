cc2efbf347ecde890d7b54c3de05b44e883b54b1
Sovisal <sunsovisal50@gmail.com>
2026-03-23 09:44:57 +0700

    Welcome Page

--- Welcome_Menu.java
+++ Welcome_Menu.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Welcome_Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
